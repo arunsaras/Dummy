627ed8291011e7b6811a5d0259dd1937c705a52f
“arun <arunsaras48.2@gmail.com>
2021-09-03 15:17:15 +0530

    initial commit 1

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+
+
+app.get('/',(req,res)=>{
+    console.dir(res) // false
+  res.send('OK')
+  console.dir(res.headersSent) // true
+});
+
+
+app.post('/',(req,res)=>{
+
+  const { body } = req;
+  const schema = Joi.object({
+    name: Joi.string().min(2).required()
+  });
+
+  const result = schema.validate(body);
+  if(result.error){
+    res.status(400).send('error');
+  }else
+  res.send(result);
+})
+
+app.listen(3000,()=>console.log('Your host 3000 is listening...'))
